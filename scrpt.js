232b95a921ddcce4b9398cba620437aef606ff58
Alison Souza <alison-cruz@hotmail.com>
2024-06-10 00:15:55 -0300

    adição da aba carrinho e algumas funções no javascript

--- scrpt.js
+++ scrpt.js
@@ -1,10 +1,54 @@
-function add(button) {
+// function add(button) {
 
-    var itemValue = button.parentNode.querySelector('.valor').value;
+//     var itemValue = button.parentNode.querySelector('.valor').value;
     
-    var carrinho = document.getElementById('carrinho');
-    var carrinhoValue = parseFloat(carrinho.value);
+//     var carrinho = document.getElementById('carrinho');
+//     var carrinhoValue = parseFloat(carrinho.value);
     
 
-    carrinho.value = (carrinhoValue + parseFloat(itemValue)).toFixed(2);
-}
+//     carrinho.value = (carrinhoValue + parseFloat(itemValue)).toFixed(2);
+// }
+document.querySelectorAll('.add').forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('car-aba').style.display = 'flex';
+    });
+});
+
+document.querySelectorAll('.fechar').forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('car-aba').style.display = 'none';
+        document.getElementById('car').style.display = 'flex';
+    });
+});
+
+document.querySelectorAll('#car').forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('car-aba').style.display = 'flex';
+        document.getElementById('car').style.display = 'none';
+    });
+});
+
+let quantidaden = 1;
+document.querySelector('.quantidade').value = quantidaden;
+
+document.querySelectorAll('#minus').forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (quantidaden == 1) {
+            document.getElementById('car-aba').style.display = 'none';
+        } else{
+            quantidaden--;
+            document.querySelector('.quantidade').value = quantidaden;
+        }
+        console.log(quantidaden);
+    });
+});
+
+document.querySelectorAll('#plus').forEach(button => {
+    button.addEventListener('click', (e) => {
+       quantidaden++;
+       document.querySelector('.quantidade').value = quantidaden;
+    });
+});
