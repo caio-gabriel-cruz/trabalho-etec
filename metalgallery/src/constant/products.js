28bfc9b399f69cac01d132f4d54d257246731a77
Alison Souza <alison@contasimples.com>
2024-06-16 16:02:19 -0300

    feat: implementa nova arquitetura e atualiza somatória do valor total do carrinho

---
+++ metalgallery/src/constant/products.js
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    image: { alt: "alarga", url: "./src/assets/images/produtos/produto1.webp" },
+    name: "Alargabrinco preto",
+    value: 39.99,
+  },
+  {
+    image: { alt: "alarga", url: "./src/assets/images/produtos/produto1.webp" },
+    name: "Alargabrinco preto",
+    value: 39.99,
+  },
+  {
+    image: { alt: "alarga", url: "./src/assets/images/produtos/produto1.webp" },
+    name: "Alargabrinco preto",
+    value: 39.99,
+  },
+  {
+    image: { alt: "alarga", url: "./src/assets/images/produtos/produto1.webp" },
+    name: "Alargabrinco preto",
+    value: 39.99,
+  },
+];
